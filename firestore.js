b9219869c55e72d7e3b5e76c00edda26f222723f
VaishnaviVhora10 <86514711+VaishnaviVhora10@users.noreply.github.com>
2022-03-07 10:51:20 +0530

    Add files via upload

---
+++ firestore.js
@@ -0,0 +1,19 @@
+import { getFirestore } from 'firebase/firestore'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey : process.env.FIREBASE_API,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MESSUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+const analytics = getAnalytics(app);
+ 
+export default db;
